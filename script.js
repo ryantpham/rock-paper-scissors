82922a004b010863d44e92abb67e3b46489a0471
Ryan Pham <rtpham10@gmail.com>
2024-02-10 15:10:30 -0800

    Bug fixes and added feature: fixed return bug in playRound() function and created a new function for player choice

--- script.js
+++ script.js
@@ -1,51 +1,90 @@
+let playerPoints = 0;
+let computerPoints = 0;
+const choices = ["Rock","Paper","Scissors"];
+
+let win = "You Win!";
+let lose = "You Lose!";
+let tie = "It is a Tie";
+
+let playerSelection;
+let computerSelection;
+
+function game(){
+    //runs the game
+    //plays 3 rounds
+    //console based
+    playerSelection = playerChoice();
+    computerSelection = getComputerChoice();
+    if (playerPoints < 3 && computerPoints < 3){
+        if( playRound() == win ){
+            playerPoints ++;
+            console.log(`You picked: ${playerSelection}, Computer picked: ${computerSelection}`);
+            console.log(`Your score: ${playerPoints} Computer score: ${computerPoints}`);
+        } else if( playRound() == lose ){
+            computerPoints ++;
+            console.log(`You picked: ${playerSelection}, Computer picked: ${computerSelection}`);
+            console.log(`Your score: ${playerPoints} Computer score: ${computerPoints}`);
+        } else if ( playRound() == tie ){
+            console.log(`You picked: ${playerSelection}, Computer picked: ${computerSelection}`);
+            console.log (`Scores remain the same - Player:Computer ${playerPoints}:${computerPoints}`);
+        }
+    } else if ( playerPoints > computerPoints ) {
+        console.log("You won the game!")
+    } else {
+        console.log ("You lost the game.")
+    }
+}
+
 //function randomly selects Rock, Paper, or Scissor from choices array
 function getComputerChoice(){
-    const choices = ["Rock","Paper","Scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex]
 }
 
+function playerChoice(){
+    //gets input from player
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    return playerSelection
+}
 
 //function runs the game of rock paper scissor once
-function playRound(playerSelection, computerSelection){
-
+function playRound(){
     // Convert playerSelection to match the case of the choices
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-
     if (computerSelection === playerSelection) {
-        console.log("It is a Tie");
+        return "It is a Tie";
     } else if (computerSelection === "Rock" && playerSelection === "Paper") {
-        console.log("You Win!");
+        return "You Win!";
     } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
-        console.log("You Win!");
+        return "You Win!";
     } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
-        console.log("You Win!");
+        return "You Win!";
     } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
-        console.log("You Lose!");
+        return "You Lose!";
     } else if (computerSelection === "Paper" && playerSelection === "Rock") {
-        console.log("You Lose!");
+        return "You Lose!";
     } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
-        console.log("You Lose!");
+        return "You Lose!";
     } else {
-        console.log("Invalid input. Please enter Rock, Paper, or Scissors.");
+        return "Invalid input. Please enter Rock, Paper, or Scissors.";
     }    
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+//game();
+
+
+
+
+
+/*
+
+//Test Code for Computer Choice
+playerSelection = "rock";
+computerSelection = getComputerChoice();
 console.log("You: " + playerSelection)
 console.log( "Computer: " + computerSelection)
 
 //Runs the Game once using parameters
 console.log(playRound(playerSelection, computerSelection));
 
-//GOAL: playGame(). play a five round game that keeps score and reports a winner or loser at the end.
-//let playerSelection = prompt("Rock, Paper, Scissors?") <-- This stores user input
-
-function playGame(){
-    let result = playRound();
-
-}
-
-
-
+*/
