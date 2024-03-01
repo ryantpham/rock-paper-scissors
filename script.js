767279fde6d6fb941772a3a9d602adcdd75a5f62
Ryan Pham <rtpham10@gmail.com>
2024-02-29 23:20:23 -0800

    Created selection results and scoreboard

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let computerPoints = 0;
 const choices = ["Rock","Paper","Scissors"];
 let win = "You Win!";
 let lose = "You Lose!";
-let tie = "It is a Tie";
+let tie = "Tie!";
 let playerSelection;
 let computerSelection;
 
@@ -14,10 +14,6 @@ let computerSelection;
 //plays 3 rounds
 //console based
 function game(){
-    playerSelection = playerChoice();
-    computerSelection = getComputerChoice();
-    let result = playRound();
-
     //runs the game and keeps track with a counter
     if( playRound() == win ){
         playerPoints ++;
@@ -33,7 +29,7 @@ function game(){
     }
 
     //Checks if the game should continue as long as the points are below 3
-    if ( playerPoints < 1 && computerPoints < 1 ){
+    if ( playerPoints <= 0 && computerPoints <= 0 ){
         game(); //This is a recursive call which the function calls itself if the if statement is satisfied
     } else if (playerPoints > computerPoints) {
         console.log("You won the game!")
@@ -49,9 +45,9 @@ function getComputerChoice(){
 }
 
 
-function playerChoice(){
+function playerChoice(x){
     //gets input from player
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    let playerSelection = x;
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     return playerSelection
 }
@@ -59,21 +55,27 @@ function playerChoice(){
 //function runs the game of rock paper scissor once
 function playRound(){
     if (computerSelection === playerSelection) {
-        return "It is a Tie";
+        return "Tie!";
     } else if (computerSelection === "Rock" && playerSelection === "Paper") {
+        playerPoints ++;
         return "You Win!";
     } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+        playerPoints ++;
         return "You Win!";
     } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+        playerPoints ++;
         return "You Win!";
     } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        computerPoints ++;
         return "You Lose!";
     } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        computerPoints ++;
         return "You Lose!";
     } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        computerPoints ++;
         return "You Lose!";
     } else {
-        return "Invalid input. Please enter Rock, Paper, or Scissors.";
+        return "ERROR!";
     }    
 }
 
@@ -82,14 +84,52 @@ function playRound(){
 
 //button when clicked on
 const pickButton = document.querySelectorAll('button');
+const playerDisplay = document.querySelector('.playerDisplay');
+const computerDisplay = document.querySelector('.computerDisplay');
+const resultDisplay = document.querySelector('.resultDisplay');
+const scoreDisplay = document.querySelector('.scoreDisplay');
 
 pickButton.forEach((button) => {
     button.addEventListener('click',()=>{
-        console.log('im clicked')
-        
         if (button.classList.contains('rock')){
-            return console.log('rock')
+            const choice = 'rock';
+            playerSelection = playerChoice(choice);
+        } else if (button.classList.contains('paper')){
+            const choice = 'paper';
+            playerSelection = playerChoice(choice);
+        } else if (button.classList.contains('scissors')){
+            const choice = 'scissors';
+            playerSelection = playerChoice(choice);
         }
+
+        computerSelection = getComputerChoice();
+        result = playRound();
+        
+
+        playerDisplay.textContent = `Player: ${playerSelection}`;
+        computerDisplay.textContent = `Computer: ${computerSelection}`;
+        scoreDisplay.textContent = `Player: ${playerPoints} Computer: ${computerPoints}`
+        if (playerPoints === 3){
+            playerPoints = 0;
+            computerPoints = 0;
+            resultDisplay.textContent = `You Win!`
+            scoreDisplay.textContent = `Player: ${playerPoints} Computer: ${computerPoints}`
+
+
+        } else if (computerPoints === 3){
+            
+            playerPoints = 0;
+            computerPoints = 0;
+            resultDisplay.textContent = `You Lose!`
+            scoreDisplay.textContent = `Player: ${playerPoints} Computer: ${computerPoints}`
+
+
+        } else if (playerPoints > 3 || playerPoints > 3){
+            playerPoints = 0;
+            computerPoints = 0;
+            resultDisplay.textContent = '';
+        }
+        
 
     });
 });
